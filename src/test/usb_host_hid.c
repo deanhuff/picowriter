0dd7bc4e1b30afc0d84ae2d58e88ae4f5e6a000d
deanhuff <deanhuffman@gmail.com>
2021-08-06 09:47:22 -0400

    this builds a demo that will blink the on-board led for each keypress

--- src/test/usb_host_hid.c
+++ src/test/usb_host_hid.c
@@ -1,21 +1,80 @@
+/* 
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ha Thach (tinyusb.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#include <stdlib.h>
 #include <stdio.h>
-#include "pico/stdlib.h"
+#include <string.h>
 
-int main(){
+#include "bsp/board.h"
+#include "tusb.h"
 
-    const uint LED_PIN = 25;
+void print_greeting(void);
+void led_blinking_task(void);
+extern void hid_app_task(void);
 
-    setup_default_uart();
-    printf("Hello, world!\n");
+int main(void) {
+    board_init();
+    print_greeting();
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    tusb_init();
 
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+    while (1) {
+        // tinyusb host task
+        tuh_task();
+        //led_blinking_task();
+
+#if CFG_TUH_HID_KEYBOARD || CFG_TUH_HID_MOUSE
+        hid_app_task();
+#endif
     }
 
+    return 0;
+}
+
+//--------------------------------------------------------------------+
+// BLINKING TASK
+//--------------------------------------------------------------------+
+void led_blinking_task(void) {
+    const uint32_t interval_ms = 250;
+    static uint32_t start_ms = 0;
+
+    static bool led_state = false;
+
+    // Blink every interval ms
+    if (board_millis() - start_ms < interval_ms) return; // not enough time
+    start_ms += interval_ms;
+
+    board_led_write(led_state);
+    led_state = 1 - led_state; // toggle
+}
+
+//--------------------------------------------------------------------+
+// HELPER FUNCTION
+//--------------------------------------------------------------------+
+void print_greeting(void) {
+    printf("This Host demo is configured to support:\n");
+    if (CFG_TUH_HID_KEYBOARD) puts("  - HID Keyboard");
+    if (CFG_TUH_HID_MOUSE) puts("  - HID Mouse");
 }
